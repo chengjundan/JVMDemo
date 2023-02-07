313cc84d3073eeae3aff8aac46855285afaa610e
chengjundan <cjd8510@163.com>
2023-02-07 14:06:14 +0800

    第一次提交JVM测试项目

---
+++ src/test/java/com/lizi/JVM/JvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizi.JVM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
